73598717a6f9759a0ddd407df1675ebd78f7a91e
所无聊 <2409277719@qq.com>
2023-10-04 11:55:20 +0800

    delete dist

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PACKAGE_ROOT = exports.PROJECT_ROOT = void 0;
-const path_1 = require("path");
-/* 应用根路径 */
-exports.PROJECT_ROOT = (0, path_1.join)(__dirname, "..");
-exports.PACKAGE_ROOT = (0, path_1.join)(exports.PROJECT_ROOT, "package.json");
